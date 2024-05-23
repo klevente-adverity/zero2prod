108804eb333207258fbf57ed9f1693534241691d
klevente-adverity <levente.kersanszki@adverity.com>
2024-05-23 20:56:32 +0200

    Added the subscription endpoint to the service

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,21 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    name: String,
+    email: String
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
         })
         .listen(listener)?
         .run();
