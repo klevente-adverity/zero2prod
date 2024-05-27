ff043c275a38c497401aa34682c60fe2567c1ee8
klevente-adverity <levente.kersanszki@adverity.com>
2024-05-27 19:04:52 +0200

    Added thread pool creation for database connections

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 
+use sqlx::PgPool;
+
 use zero2prod::configurations::get_configuration;
 use zero2prod::startup::run;
 
@@ -8,5 +10,8 @@ async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(&address)?;
-    run(listener)?.await
+    let pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    run(listener, pool)?.await
 }
